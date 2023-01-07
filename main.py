7f580fa8f892f124fbf52252f28ec24b833113b7
mrlider <dva.lider@gmail.com>
2023-01-07 13:09:20 +0800

    Добавил Яндекс погоду и для других населенных пунктов и исправил ошибки

--- main.py
+++ main.py
@@ -23,68 +23,62 @@ config_dict['language'] = 'ru'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+lesogorsk = types.KeyboardButton("Лесогорск")
+moscow = types.KeyboardButton("Москва")
+chsinau = types.KeyboardButton("Кишинёв")
+sochi = types.KeyboardButton("Сочи")
+other = types.KeyboardButton("Другой")
+markup.add(moscow, sochi, chsinau, lesogorsk, other)
 
 # Обработчик команды старт
 @bot.message_handler(commands=['start'])
 def echo_all(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    lesogorsk = types.KeyboardButton("Лесогорск")
-    moscow = types.KeyboardButton("Москва")
-    chsinau = types.KeyboardButton("Кишинёв")
-    sochi = types.KeyboardButton("Сочи")
-    other = types.KeyboardButton("Другой")
-    markup.add( moscow, sochi, chsinau,  lesogorsk, other)
     bot.send_message(message.chat.id, "Выберите населённый пункт", reply_markup=markup)
 
 @bot.message_handler()
-# Обработчик сервер Яндекс
+# Обработчик сообщений
 def main(message):
     if message.text == "Лесогорск":
         lat = 56.05305
         lon = 99.53259
-        post = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_ya = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_owm = owm_wather(message.text)
+
     elif message.text == "Москва":
         lat = 55.67827
         lon = 37.53719
-        post = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_ya = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_owm = owm_wather(message.text)
     elif message.text == "Кишинёв":
         lat = 46.88650
         lon = 28.99194
-        post = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_ya = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_owm = owm_wather(message.text)
+
 
     elif message.text == "Сочи":
         lat = 43.593232
         lon = 39.727434
-        post = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_ya = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        post_owm = owm_wather(message.text)
+
     elif message.text == "Другой":
+        post_owm = ".."
+        post_ya = "Введите название населённого пункта"
 
-        post = ""
+
     else:
-        lat = geo_pos(message.text)[0]
-        lon = geo_pos(message.text)[1]
-        post = yandex_weather(lat, lon, message.text, YA_TOKEN)
+        try:
+            lat = geo_pos(message.text)[0]
+            lon = geo_pos(message.text)[1]
+            post_ya = yandex_weather(lat, lon, message.text, YA_TOKEN)
+            post_owm = owm_wather(message.text)
+        except NotFoundError:
+            post_owm = "Населённый пункт не найден"
+            post_ya = "Введите название населённого пункта"
 
-
-    other(message)
-    bot.send_message(message.chat.id, post)
-
-# Обработчик сервер OpenWeather
-def other(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    lesogorsk = types.KeyboardButton("Лесогорск")
-    moscow = types.KeyboardButton("Москва")
-    chsinau = types.KeyboardButton("Кишинёв")
-    sochi = types.KeyboardButton("Сочи")
-    other = types.KeyboardButton("Другой")
-
-    markup.add( moscow, sochi, chsinau,  lesogorsk, other)
-    try:
-        post = owm_wather(message.text)
-    except NotFoundError:
-        post = "Введите название населённого пункта"
-
-
-    bot.send_message(message.chat.id, post, reply_markup=markup)
+    bot.send_message(message.chat.id, post_owm)
+    bot.send_message(message.chat.id, post_ya, reply_markup=markup)
 
 bot.polling(none_stop = True)
